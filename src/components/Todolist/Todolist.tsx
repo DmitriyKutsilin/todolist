0ccef677231ede9d3deec211bfecbb5638fde93b
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-06-20 21:57:02 +0300

    Add removeTask and filterTask functions for buttons in Todolist

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,13 +1,19 @@
 // @flow
 import * as React from 'react';
-import {TaskType} from "../../App";
+import {FilterType, TaskType} from "../../App";
 import {Button} from "../Button/Button";
 
 type TodolistProps = {
     title: string
     tasks: TaskType[]
+    removeTask: (taskId: number) => void
+    filterTasks: (filter: FilterType) => void
 };
-export const Todolist = ({title, tasks}: TodolistProps) => {
+export const Todolist = ({title,
+                          tasks,
+                          removeTask,
+                          filterTasks}: TodolistProps) => {
+
     return (
         <div>
             <h3>{title}</h3>
@@ -20,19 +26,24 @@ export const Todolist = ({title, tasks}: TodolistProps) => {
                     tasks.length === 0
                         ? <p>Тасок нет</p>
                         : tasks.map(task => {
+                            const removeTaskHandler = () => {
+                                removeTask(task.id)
+                            }
+
                             return (
                                 <li key={task.id}>
                                     <input type="checkbox" checked={task.isDone}/>
                                     <span>{task.title}</span>
+                                    <Button title={'x'} onClick={removeTaskHandler}/>
                                 </li>
                             )
                         })
                 }
             </ul>
             <div>
-                <Button title={'All'}/>
-                <Button title={'Active'}/>
-                <Button title={'Completed'}/>
+                <Button title={'All'} onClick={() => filterTasks('all')}/>
+                <Button title={'Active'}  onClick={() => filterTasks('active')}/>
+                <Button title={'Completed'}  onClick={() => filterTasks('completed')}/>
             </div>
         </div>
     );

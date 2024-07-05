9faf73e5034f66f4bb5878997134461246c4177b
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-07-05 21:32:00 +0300

    add functionality for adding tasks by button and Enter key

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -2,24 +2,48 @@
 import * as React from 'react';
 import {FilterType, TaskType} from "../../App";
 import {Button} from "../Button/Button";
+import {ChangeEvent, useState, KeyboardEvent} from "react";
 
 type TodolistProps = {
     title: string
     tasks: TaskType[]
-    removeTask: (taskId: number) => void
+    removeTask: (taskId: string) => void
     filterTasks: (filter: FilterType) => void
+    addTask: (title: string) => void
 };
-export const Todolist = ({title,
-                          tasks,
-                          removeTask,
-                          filterTasks}: TodolistProps) => {
+export const Todolist = ({
+                             title,
+                             tasks,
+                             removeTask,
+                             filterTasks,
+                             addTask
+                         }: TodolistProps) => {
+
+    const [taskTitle, setTaskTitle] = useState<string>('')
+
+    const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement> ) => {
+        setTaskTitle(e.currentTarget.value)
+    }
+
+    const addTaskHandler = () => {
+        if (taskTitle.trim()) {
+            addTask(taskTitle)
+        }
+        setTaskTitle('')
+    }
+
+    const addTaskOnKeyDownHandler = (e: KeyboardEvent<HTMLInputElement> ) => {
+        if (e.key === 'Enter') {
+            addTaskHandler()
+        }
+    }
 
     return (
         <div>
             <h3>{title}</h3>
             <div>
-                <input/>
-                <Button title={'+'}/>
+                <input value={taskTitle} onChange={changeTaskTitleHandler} onKeyDown={addTaskOnKeyDownHandler}/>
+                <Button title={'+'} onClick={addTaskHandler}/>
             </div>
             <ul>
                 {
@@ -42,8 +66,8 @@ export const Todolist = ({title,
             </ul>
             <div>
                 <Button title={'All'} onClick={() => filterTasks('all')}/>
-                <Button title={'Active'}  onClick={() => filterTasks('active')}/>
-                <Button title={'Completed'}  onClick={() => filterTasks('completed')}/>
+                <Button title={'Active'} onClick={() => filterTasks('active')}/>
+                <Button title={'Completed'} onClick={() => filterTasks('completed')}/>
             </div>
         </div>
     );

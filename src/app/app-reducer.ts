2fe46bf0d1d567f261f6f3e8a690ccf3a6674cc7
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-10-14 22:46:42 +0300

    add initializing key

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -3,7 +3,8 @@ import {ThunkDispatch} from "redux-thunk";
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType,
+    isInitialized: false,
+    status: 'idle' as RequestStatusType,
     error: null as string | null
 }
 
@@ -13,6 +14,8 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
             return {...state, status: action.payload.status}
         case "APP/SET-ERROR":
             return {...state, error: action.payload.error}
+        case "IS-INITIALIZED":
+            return {...state, isInitialized: action.payload.isInitialized}
         default:
             return state
     }
@@ -20,12 +23,17 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
 
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', payload: {status}} as const)
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', payload: {error}} as const)
-
+export const setIsInitializedAC = (isInitialized: boolean) => ({
+    type: 'IS-INITIALIZED',
+    payload: {isInitialized}
+} as const)
 
 type InitialStateType = typeof initialState
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetIsInitializedActionType = ReturnType<typeof setIsInitializedAC>
 
 type ActionsType =
     | SetAppStatusActionType
     | SetAppErrorActionType
+    | SetIsInitializedActionType

36900224cd02a918e24d2d770211b01c91c78cd8
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-10-14 22:46:22 +0300

    add auth-reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,20 @@
 import {applyMiddleware, combineReducers, legacy_createStore, UnknownAction} from "redux";
 import {tasksReducer} from "../features/TodolistsList/tasks-reducer";
 import {todolistsReducer} from "../features/TodolistsList/todolists-reducer";
-import {thunk, ThunkDispatch} from "redux-thunk";
+import {thunk, ThunkDispatch, ThunkMiddleware} from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {appReducer} from "./app-reducer";
+import {authReducer} from "../features/Login/auth-reducer";
+
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
+    auth: authReducer,
 })
+
+//TODO спросить о ошибке legacy_createStore
 
 // @ts-ignore
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));

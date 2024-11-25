20962540e4408d59838559674a6095b6a5862b2f
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-11-25 13:44:28 +0300

    add TodolistTitle component and styles module

---
+++ src/features/TodolistsList/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -0,0 +1,39 @@
+// @flow
+import * as React from 'react'
+import { deleteTodolistTC, TodolistDomainType, updateTodolistTC } from 'features/TodolistsList/todolistsSlice'
+import { EditableSpan } from 'components/EditableSpan/EditableSpan'
+import IconButton from '@mui/material/IconButton/IconButton'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import { useAppDispatch } from 'app/store'
+import s from './TodolistTitle.module.css'
+
+type Props = {
+  todolist: TodolistDomainType
+}
+
+export const TodolistTitle = ({ todolist }: Props) => {
+  const { title, id, entityStatus } = todolist
+  const dispatch = useAppDispatch()
+
+  const updateTodolistHandler = (title: string) => {
+    dispatch(updateTodolistTC(id, title))
+  }
+
+  const removeTodolistHandler = () => {
+    dispatch(deleteTodolistTC(id))
+  }
+
+  return (
+    <div className={s.todolistTitleContainer}>
+      <EditableSpan
+        className={s.todolistHeader}
+        value={title}
+        onChange={updateTodolistHandler}
+        disabled={entityStatus === 'loading'}
+      />
+      <IconButton onClick={removeTodolistHandler} disabled={entityStatus === 'loading'}>
+        <DeleteForeverIcon />
+      </IconButton>
+    </div>
+  )
+}

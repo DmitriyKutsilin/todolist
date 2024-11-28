f1db17f6c60ba047bbe1719a4ecd07d7b7c294c6
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-11-28 21:06:11 +0300

    refactoring

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,50 +1,36 @@
-import { createBrowserRouter, Navigate, RouteObject } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import App from '../app/App'
 import { Login } from 'features/Login/Login'
-import { TodolistsList } from 'features/todolists/ui/TodolistsList/TodolistsList'
 import React from 'react'
-import { PrivateRoutes } from './PrivateRoutes'
 import { Page404 } from 'common/components/Page404/Page404'
+import { Main } from 'app/Main'
 
 export const PATH = {
-  TODOLISTS: '/todolists',
   LOGIN: '/login',
-  ERROR: '*',
-}
-
-const privateRoutes: RouteObject[] = [
-  {
-    index: true,
-    element: <Navigate to={PATH.TODOLISTS} />,
-  },
-  {
-    path: PATH.TODOLISTS,
-    element: <TodolistsList />,
-  },
-]
-
-const publicRoutes: RouteObject[] = [
-  {
-    path: PATH.LOGIN,
-    element: <Login />,
-  },
-]
+} as const
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Navigate to={PATH.ERROR} />,
     children: [
       {
-        element: <PrivateRoutes />,
-        children: privateRoutes,
+        path: '/',
+        element: <Main />,
       },
-      ...publicRoutes,
+      {
+        path: PATH.LOGIN,
+        element: <Login />,
+      },
+      {
+        path: '*',
+        element: <Page404 />,
+      },
     ],
   },
-  {
-    path: PATH.ERROR,
-    element: <Page404 />,
-  },
 ])
+
+//   {
+//     index: true,
+//     element: <Navigate to={PATH.TODOLISTS} />,
+// }

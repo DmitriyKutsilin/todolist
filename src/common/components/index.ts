2e992d67ffc2c6225cf7230f6257fc15c79973b2
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-11-28 14:54:08 +0300

    add index file for components

---
+++ src/common/components/index.ts
@@ -0,0 +1,6 @@
+export { EditableSpan } from './EditableSpan/EditableSpan'
+export { AddItemForm } from './AddItemForm/AddItemForm'
+export { ButtonWithMemo } from './Button/ButtonWithMemo'
+export { Header } from './Header/Header'
+export { ErrorSnackbar } from './ErrorSnackbar/ErrorSnackbar'
+export { Page404 } from './Page404/Page404'

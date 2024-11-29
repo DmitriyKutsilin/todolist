e30eecd82d68846c9a844513c6cbf4c7ee5e195c
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-11-29 19:38:00 +0300

    refactoring

--- src/features/auth/model/authSlice.ts
+++ src/features/auth/model/authSlice.ts
@@ -13,13 +13,11 @@ export const authSlice = createSlice({
   initialState: {
     isLoggedIn: false,
   },
-  reducers: (create) => {
-    return {
-      setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
-        state.isLoggedIn = action.payload.isLoggedIn
-      }),
-    }
-  },
+  reducers: (create) => ({
+    setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn
+    }),
+  }),
   selectors: {
     selectIsLoggedIn: (state) => state.isLoggedIn,
   },

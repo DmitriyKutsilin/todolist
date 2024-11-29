677503dc53ebc2aff509bad08a49ee5f4dca1e21
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-11-29 20:18:28 +0300

    move custom hooks from store to own files

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,6 @@ import { UnknownAction } from 'redux'
 import { tasksReducer, tasksSlice } from 'features/todolists/model/tasksSlice'
 import { todolistsReducer, todolistsSlice } from 'features/todolists/model/todolistsSlice'
 import { ThunkAction } from 'redux-thunk'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { appReducer, appSlice } from 'app/appSlice'
 import { authReducer, authSlice } from 'features/auth/model/authSlice'
 import { configureStore } from '@reduxjs/toolkit'
@@ -19,6 +18,3 @@ export const store = configureStore({
 export type AppRootStateType = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>
 export type AppDispatch = typeof store.dispatch
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
